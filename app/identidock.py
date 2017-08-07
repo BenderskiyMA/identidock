e0c15267569bedfa8fe1c1ee09509dfb995e4620
root <olmer@remlo.ru>
2017-08-08 00:42:55 +0700

    изменено:      app/identidock.py 	изменено:      cmd.sh 	изменено:      docker-compose.yml 	изменено:      jenkins.yml

--- app/identidock.py
+++ app/identidock.py
@@ -44,5 +44,8 @@ def get_identicon ( name ):
     return Response( image, mimetype='image/png')
 
 
+
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
+
+
--- cmd.sh
+++ cmd.sh
@@ -10,6 +10,7 @@ elif [ "$ENV" = 'UNIT'  ]; then
    exec python "tests.py"
 else
    echo "Running Production server"
-   exec uwsgi --http 0.0.0.0:9090 -wsgi-file /app/identidock.py --callable app --stats 0.0.0.0:9191
+#   exec uwsgi --http 0.0.0.0:9090 -wsgi-file /app/identidock.py --callable app --stats 0.0.0.0:9191
+   exec uwsgi --http 0.0.0.0:9090 --wsgi-file /app/identidock.py --callable app --stats 0.0.0.0:9191
 fi
 
